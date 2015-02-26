2343334879d4fa3a7f1eacadbb53338cbe31d486
Justin Gagnon <gagnon_justin@hotmail.com>
2015-02-26 15:34:07 -0500

    Updated ticTacToe.php
    Created the form for choosing the players piece (X or O).
    Set the piece chosen from the form to the player class.

--- ticTacToe.php
+++ ticTacToe.php
@@ -1,16 +1,39 @@
-<?php require("board.php"); ?>
+<?php
+require("board.php");
+require("players.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8"/>
-        <title>Tic Tac Toe</title>
-    </head>
-    <body>
-        <h1>Tic Tac Toe</h1>
-        <?php
-            $board = new board();
-            $board->initialize();
-            //$board->displayBoard();
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Tic Tac Toe</title>
+</head>
+<body>
+<h1>Tic Tac Toe</h1>
+<?php
+$board = new board();
+$board->displayBoard();
+?>
+<form method="get" action="ticTacToe.php">
+    <input type="radio" name="piece" value="x" /> X <br/>
+    <input type="radio" name="piece" value="o" /> O
+    <input type="submit" name="submit" value="Pick your piece!" />
+</form>
+<?php
+    if (isset($_GET['submit'])) {
+        if (isset($_GET['piece'])) {
+            $player1 = new player($_GET['piece']);
+            //echo "Player 1 picked " . $player1->playerNum;
+            if ($player1->playerNum == 'x') {
+                $player2 = new player('o');
+            } else {
+                $player2 = new player('x');
+            }
+            //echo "Player 2 picked " . $player2->playerNum;
+        } else {
+            echo "Please select a piece.";
+        }
+    }
+?>
+</body>
 </html>
