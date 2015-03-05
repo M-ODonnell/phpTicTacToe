4637fde7312881c3f43d707f80f3936118e89ea2
Justin Gagnon <gagnon_justin@hotmail.com>
2015-03-05 15:53:42 -0500

    Finished working on board.php
    Created functions getCurrentPlayer and assignTakenTurn to handle players not selecting a piece so that the turns do not change unless a choice is selected.

--- board.php
+++ board.php
@@ -1,5 +1,7 @@
 <?php
 class board {
+    public $player1;
+    public $player2;
     public $elements = array();
     function __construct() {
         for ($i = 0; $i < 9; $i++) {
@@ -37,9 +39,31 @@ class board {
             ($this->elements[0] == $player && $this->elements[4] == $player && $this->elements[8] == $player) ||
             ($this->elements[6] == $player && $this->elements[4] == $player && $this->elements[2] == $player)
         ) {
-            echo "Player ".$player." has won!<br/><br/>";
+            echo "<br/>Player ".$player." has won!<br/><br/>";
             echo "<a href='ticTacToe.php'>Start a New Game</a>";
             exit;
+        }
+    }
+
+    function getCurrentPlayer($turn) {
+        if ($turn % 2 == 0) {
+            // player two just went, so player one is active
+            echo "Active player: " . $this->player2;
+        } else {
+            // player one just went, so player two is active
+            echo "Active player: " . $this->player1;
+        }
+    }
+
+    function assignTakenTurn($turn, $choice) {
+        if ($turn % 2 == 0) {
+            $this->elements[$choice - 1] = $this->player2;
+            // check to see if the game has been won
+            $this->checkForWin($this->player2);
+        } else {
+            $this->elements[$choice - 1] = $this->player1;
+            // check to see if the game has been won
+            $this->checkForWin($this->player1);
         }
     }
 }
